3378a3e0393e1d22fe169c36fd4ff72f70d50a0d
swarkhare <swarkhare04@gmail.com>
2025-06-30 20:32:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".explore-btn").addEventListener("click", () => {
+    alert("Explore Rooms clicked!");
+  });
+  
+  document.querySelector(".book-btn").addEventListener("click", () => {
+    alert("Redirecting to booking page...");
+  });
+  
