5a63961bb22daab85ad3a9e9bf8fea364aa4c36c
swarkhare <swarkhare04@gmail.com>
2025-07-14 19:53:16 -0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.querySelector(".explore-btn").addEventListener("click", () => {
-    alert("Explore Rooms clicked!");
-  });
-  
-  document.querySelector(".book-btn").addEventListener("click", () => {
-    alert("Redirecting to booking page...");
-  });
+  document.querySelector("#accommodations").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".book-btn").addEventListener("click", () => {
+  document.querySelector("#booking-form").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".social-btn").addEventListener("click", () => {
+  alert("Don't forget to tag us in your photos on Instagram, Facebook, or Twitter!");
+});
+function toggleMode() {
+  const isDark = confirm("Would you like to switch to Dark Mode?");
+  if (isDark) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+}
+
   
